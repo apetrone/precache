d1dbf1b525a1bd3b034544c4bb4a21819fce7d31
Jason Kozak <jkozak@qnx.com>
2011-12-23 17:53:18 -0500

    Simplified premake configuration and fixed osx+gmake issues

--- premake4.lua
+++ premake4.lua
@@ -1,21 +1,11 @@
 solution "precache"
 configurations { "debug", "release" }
-	
-targetOS = "unknown";
-
-if _ACTION == "vs2005" or _ACTION == "vs2008" or _ACTION == "vs2010" then
-	targetOS = "windows"
-elseif _ACTION == "codeblocks" or _ACTION == "gmake" then
-	targetOS = "linux"
-elseif _ACTION == "xcode3" then
-	targetOS = "macosx"
-end
 
 project "precache"
 	objdir "obj"
 	targetdir "build/bin"
 	uuid( "e713b970-81b8-11e0-b278-0800200c9a66" )
-	platforms{ "x32", "x64" }
+	platforms{ "native", "x32", "x64" }
 
 	kind "WindowedApp"
 	language ("C")
@@ -23,52 +13,52 @@ project "precache"
 	
 	files
 	{
-		"src/**.c",	
-		"include/**.h",	
+		"src/**.c",
+		"include/**.h",
 	}
 
-	includedirs 
+	includedirs
 	{ 
 		"include",
-	}	
+	}
 
-	if targetOS == "windows" then
+	baseExcludes = 
+	{
+	}
+
+	excludes { baseExcludes }
+
+	configuration {"windows"}
 		-- use static runtime on Windows
 		flags { "StaticRuntime" }
-		
+
 		files
 		{
 			"resources/precache.rc",
 			"resources/resource.h"
 		}
-		
+
 		includedirs
 		{
 			"resources/"
 		}
-	end
-	
-	if targetOS == "macosx" then
-		files
-		{
-			"src/**.m"
-		}
-	end
 
-	
-	baseExcludes = 
-	{
-	}
-	
-	excludes { baseExcludes }
-	if targetOS == "windows" then
 		defines { "WIN32", "UNICODE", baseDefines }
-		
 		links
 		{
 			"opengl32"
 		}
-	elseif targetOS == "linux" then
+
+	configuration {"macosx"}
+		files
+		{
+			"src/**.m",
+			"../xwl2/src/**.m"
+		}
+
+		links { "Cocoa.framework", "OpenGL.framework" }
+
+	configuration {"linux"}
 		defines { "LINUX=1", baseDefines }
 		links
 		{
@@ -76,42 +66,31 @@ project "precache"
 			"X11",
 			"GL"
 		}
-	elseif targetOS == "macosx" then
-		defines { "__MACH__", baseDefines }
-		files
-		{
-			"../xwl2/src/**.m"
-		}
-		
-		linkoptions
-		{
-			"-framework Cocoa",
-			"-framework OpenGL"
-		}
-	end
 	
-	
-	configuration { "release" }
-		if targetOS == "macosx" then
-			postbuildcommands
-			{
-				"cp ./resources/Info.plist build/bin/precache.app/Contents/",
-				"mkdir -p build/bin/precache.app/Contents/Resources/",
-				"cp ./resources/precache.icns build/bin/precache.app/Contents/Resources/"
-			}
-		end
-
 	configuration { "debug" }
-		if targetOS == "macosx" then
+		targetsuffix "d"	
+		defines { "DEBUG" }
+		flags { "Symbols" }
+	
+	configuration { "macosx", "release" }
+		postbuildcommands
+		{
+			"cp ./resources/Info.plist build/bin/precache.app/Contents/",
+			"mkdir -p build/bin/precache.app/Contents/Resources/",
+			"cp ./resources/precache.icns build/bin/precache.app/Contents/Resources/"
+		}
+
+	-- workaround for issue w/ premake not applying the suffix to the bundle
+	configuration { "macosx", "debug", "gmake" }
+		targetname "precached"
+		targetsuffix ""
+
+	configuration { "macosx", "debug" }
 			postbuildcommands
 			{
 				"cp ./resources/Info.plist build/bin/precached.app/Contents/",
 				"mkdir -p build/bin/precached.app/Contents/Resources/",
 				"cp ./resources/precache.icns build/bin/precached.app/Contents/Resources/"
 			}
-		end
-		targetsuffix "d"		
-		defines { "DEBUG" }
-		flags { "Symbols" }
 
 	
